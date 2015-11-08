9720311e9d10dd76f105df017455567fec476555
Camden Rudisill <camden.rudisill@richmond.edu>
2015-11-08 12:30:54 -0500

    Update: initial test deploy to Heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
-
 var gulp = require('gulp'),
-	imagemin = require('gulp-imagemin'),
-	cache = require('gulp-cache');
+  imagemin = require('gulp-imagemin'),
+  cache = require('gulp-cache');
 
-	var src = 'src/';
+var src = 'src/';
 
 
 // Compress Images
-gulp.task('image', function(){
-	gulp.src('app/img/*')
-		.pipe(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('public/img'));	
-	});
+gulp.task('image', function() {
+  gulp.src('app/img/*')
+    .pipe(imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('public/img'));
+});
 
 // Watch for changes in files
 gulp.task('watch', function() {
-	// watch image files
-	gulp.watch(src + 'img/**/*', ['image']);
-	});
+  // watch image files
+  gulp.watch(src + 'img/**/*', ['image']);
+});
 
 
 
 // allows you to just type 'gulp' in terminal
-gulp.task('default', ['watch','image']); 
-
+gulp.task('default', ['watch', 'image']);
