3b0dd559276273a4c58283d5dcf58ff862c632c7
Camden Rudisill <camden.rudisill@richmond.edu>
2015-11-22 19:07:00 -0500

    Add: stock D3 chart

--- app/components/BarChartContainer.jsx
+++ app/components/BarChartContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BarChart } from 'react-d3';
 
 export default class BarChartContainer extends React.Component {
   constructor(props) {
@@ -7,18 +8,60 @@ export default class BarChartContainer extends React.Component {
   }
 
 
-  render() {
-    return (
-      <div style={container}>Bar</div>
-    );
-  }
+render() {
+  return (
+
+      <div style={container}>
+        <BarChart
+          style={barchart}
+          data={barData}
+          width={300}
+          height={200}
+          fill={'#3182bd'}
+          backgroundColor={'#fff'}
+          title='Bar Chart'
+          yAxisLabel='% of Total Calories'
+          xAxisLabel='Nutrients'
+          />
+      </div>
+
+  );
+}
 
 };
 
+const barData = [
+  {
+    'name': 'Series A',
+    'values': [
+      { 'x': 'Calories', 'y':  91},
+      { 'x': 'Protein', 'y': 61},
+      { 'x': 'Fat', 'y':  21},
+      { 'x': 'Carbs', 'y':  41},
+      { 'x': 'Sodium', 'y':  81},
+
+    ],
+  },
+];
+
+const Colors = {
+  white: '#fff',
+  black: '#000',
+};
 
 const container = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
   flex: 2,
-  backgroundColor: 'black',
+  backgroundColor: Colors.black,
   margin: 10,
+};
 
+const barchart = {
+  display: 'flex',
+  justifyContent: 'center',
+  backgroundColor: Colors.white,
+  height: 220,
+  width: 600,
 };
